660d2438ccddd0be746d719766f67853ac71254b
Bombaba <iky.hdst@gmail.com>
2018-06-18 18:25:45 +0900

    changed main.c

--- tsp.c
+++ tsp.c
@@ -839,101 +839,93 @@ void print_points(struct point pts[], int n_pts)
 bool calc_two_opt(double radius, struct point pts[], int n_pts, int tour[],
                   struct kdtree* tree, struct kdheap* heap)
 {
-  bool success = false;
-  build_list_from_tour(pts, n_pts, tour);
-  while (two_opt(pts, n_pts, radius, tree, heap)) {
-      success = true;
-      struct point* list_tour = pts;
-      for (int i = 0; i < n_pts; i++) {
-          tour[i] = list_tour->index;
-          list_tour = list_tour->next;
-      }
-      double min_length = tour_length(pts, n_pts, tour);
-      sprintf(tourFileName, "tour%08d.dat", ++num);
-      write_tour_data(tourFileName, n_pts, tour);
-      printf("\n%s: %lf\n", tourFileName, min_length);
-  }
-  return success;
+    bool success = false;
+    build_list_from_tour(pts, n_pts, tour);
+    while (two_opt(pts, n_pts, radius, tree, heap)) {
+        success = true;
+    }
+    if (success) {
+        struct point* list_tour = pts;
+        for (int i = 0; i < n_pts; i++) {
+            tour[i] = list_tour->index;
+            list_tour = list_tour->next;
+        }
+    }
+
+    return success;
 }
 
 bool calc_or_opt(int len, double radius, struct point pts[], int n_pts, int tour[],
                  struct kdtree* tree, struct kdheap* heap)
 {
-  bool success = false;
-  build_list_from_tour(pts, n_pts, tour);
-  while (or_opt(len, pts, n_pts, radius, tree, heap)) {
-      success = true;
-      struct point* list_tour = pts;
-      for (int i = 0; i < n_pts; i++) {
-          tour[i] = list_tour->index;
-          list_tour = list_tour->next;
-      }
-      double min_length = tour_length(pts, n_pts, tour);
-      sprintf(tourFileName, "tour%08d.dat", ++num);
-      write_tour_data(tourFileName, n_pts, tour);
-      printf("\n%s: %lf\n", tourFileName, min_length);
-  }
-  return success;
+    bool success = false;
+    build_list_from_tour(pts, n_pts, tour);
+    while (or_opt(len, pts, n_pts, radius, tree, heap)) {
+        success = true;
+    }
+    if (success) {
+        struct point* list_tour = pts;
+        for (int i = 0; i < n_pts; i++) {
+            tour[i] = list_tour->index;
+            list_tour = list_tour->next;
+        }
+    }
+    return success;
 }
 
 int main(int argc, char *argv[])
 {
-  int n_pts;
-  struct point pts[MAX_N];
+    int n_pts;
+    struct point pts[MAX_N];
 
-  if(argc != 2) {
-    fprintf(stderr,"Usage: %s <tsp_filename>\n",argv[0]);
-    return EXIT_FAILURE;
-  }
+    if(argc != 2) {
+        fprintf(stderr,"Usage: %s <tsp_filename>\n",argv[0]);
+        return EXIT_FAILURE;
+    }
 
-  read_tsp_data(argv[1], pts, &n_pts);
-  //print_points(pts, n_pts);
-  //putchar('\n');
+    read_tsp_data(argv[1], pts, &n_pts);
+    //print_points(pts, n_pts);
+    //putchar('\n');
 
-  int tour[MAX_N];
-  int best_tour[MAX_N];
-  double min_length = DBL_MAX;
-  struct kdtree* tree = build_kdtree(pts, n_pts);
-  struct kdheap* heap = create_kdheap(tree);
-  //print_kdtree(tree);
+    int tour[MAX_N];
+    double min_length = DBL_MAX;
+    struct kdtree* tree = build_kdtree(pts, n_pts);
+    struct kdheap* heap = create_kdheap(tree);
+    //int best_tour[MAX_N];
+    //print_kdtree(tree);
 
+    for (int start = 0; start < n_pts; start++) {
+        putchar('-');
+        fflush(stdout);
 
-  printf("\n########## Nearest Neighbor ##########\n");
-  for (int start = 0; start < n_pts; start++) {
-      build_tour_nn(pts, n_pts, start, tour, tree);
+        build_tour_nn(pts, n_pts, start, tour, tree);
 
-      putchar('-');
-      fflush(stdout);
-      double length = tour_length(pts, n_pts, tour);
-      if (length < min_length) {
-          min_length = length;
-          memcpy(best_tour, tour, sizeof(int) * n_pts);
-          sprintf(tourFileName, "tour%08d.dat", ++num);
-          write_tour_data(tourFileName, n_pts, best_tour);
-          printf("\n%s: %lf\n", tourFileName, length);
+        bool success;
+        int count = 0;
+        do {
+            success = false;
+            success |= calc_two_opt(5*count, pts, n_pts, tour, tree, heap);
+            for (int i = 1; i <= 10; i++) {
+                success |= calc_or_opt(i, 5*count, pts, n_pts, tour, tree, heap);
+            }
+            count++;
+        } while (success);
 
-      }
-  }
+        double length = tour_length(pts, n_pts, tour);
+        if (length < min_length) {
+            min_length = length;
+            //memcpy(best_tour, tour, sizeof(int) * n_pts);
+            sprintf(tourFileName, "tour%08d.dat", ++num);
+            write_tour_data(tourFileName, n_pts, tour);
+            printf("\n%s: %lf\n", tourFileName, min_length);
 
-  bool success;
-  int count = 0;
-  do {
-      printf("\n########## Opt #%d ##########\n", count);
+        }
+    }
 
-      success = false;
+    free_kdtree(tree);
+    free_kdheap(heap);
+    printf("\n");
 
-      success = calc_two_opt(10*count, pts, n_pts, best_tour, tree, heap);
-      for (int i = 1; i <= 10; i++) {
-          success |= calc_or_opt(i, 10*count, pts, n_pts, best_tour, tree, heap);
-      }
-      count++;
-  } while (success);
-
-
-  free_kdtree(tree);
-  free_kdheap(heap);
-  printf("\n");
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
