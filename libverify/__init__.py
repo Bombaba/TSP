3c1578a5ea9775d84249f94d42f377d91bad6b18
Bombaba <iky.hdst@gmail.com>
2018-07-10 18:07:04 +0900

    fixed a bug when the execute file doesn't have suffix

--- libverify/__init__.py
+++ libverify/__init__.py
@@ -4,11 +4,12 @@ from pathlib import Path
 OUT_NAME = "checkdat"
 
 dir_path = Path(__path__[0])
-c_path = dir_path / Path("check_validity_prec.c")
-out_path = dir_path / Path(OUT_NAME)
+c_path = dir_path / "check_validity_prec.c"
+out_path = dir_path / OUT_NAME
 
-process = subprocess.run(["gcc", str(c_path), "-lm", "-o", str(out_path)], check=True)
-print("check_validity build success")
+if not out_path.exists():
+    process = subprocess.run(["gcc", str(c_path), "-lm", "-o", str(out_path)], check=True)
+    print("check_validity build success")
 
 def check(tsp_path, dat_path):
     process = subprocess.run([str(out_path), str(dat_path), str(tsp_path)],
